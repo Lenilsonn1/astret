73feea3e366481c36e68c2da09e9c5f362251e2d
Lenilson Lucas <89927279+Lenilsonn1@users.noreply.github.com>
2023-01-04 06:35:36 -0300

    atualizações da pagina

---
+++ projetoCurso/login/js.js
@@ -0,0 +1,14 @@
+var validar = document.querySelector('#login')
+var nome = document.querySelector('#usuario')
+
+validar.addEventListener('click')
+
+function validarLogin (){
+
+    if (nome == 'lucas') {
+        
+        
+    } else {
+        alert('oi')
+    }
+}
