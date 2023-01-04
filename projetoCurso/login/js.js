0e88b867fab74ab075ffc41f6eda57f4ce57211b
Lenilson Lucas <89927279+Lenilsonn1@users.noreply.github.com>
2023-01-04 12:29:00 -0300

    Atualizando projeto da astrit

--- projetoCurso/login/js.js
+++ projetoCurso/login/js.js
@@ -1,14 +1,15 @@
 var validar = document.querySelector('#login')
-var nome = document.querySelector('#usuario')
 
 validar.addEventListener('click')
 
 function validarLogin (){
+    var nome = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
 
-    if (nome == 'lucas') {
-        
-        
+    if (nome === 'lenilson' && senha === 'lucas123') {
+        console.log('bem vindo', nome); 
+        window.open('http://127.0.0.1:5500/home/home.html') 
     } else {
-        alert('oi')
+        console.error('login e senha invalido');
     }
 }
